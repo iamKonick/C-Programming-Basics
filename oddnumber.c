1325f17835de93e81af63f7f86d670cb50a73c2c
Md. Efthakhar Ul Alam <67385172+iamKonick@users.noreply.github.com>
2020-06-24 23:37:04 +0600

    Even Number Print From 1 to 10, Comment update.

---
+++ oddnumber.c
@@ -0,0 +1,27 @@
+/**
+ * C program to display all odd numbers between 1 to n without using if statement
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+
+    /* Input upper limit from user */
+    printf("Print odd numbers till: ");
+    scanf("%d", &n);
+
+    printf("All odd numbers from 1 to %d are: \n", n);
+
+    /*
+     * Start a loop from 1, increment it by 2.
+     * For each repetition prints the number.
+     */
+    for(i=1; i<=n; i+=2)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
